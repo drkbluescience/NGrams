ecbf1a459f0f199b719300461a3f4126c696c336
enyse <zngnenise@gmail.com>
2023-05-11 01:14:07 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(31, 105);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(339, 563);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(471, 105);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(339, 563);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Location = new System.Drawing.Point(894, 105);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(339, 563);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(28, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "1-gram";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(465, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "2-gram";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(888, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "3-gram";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(28, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 18);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(888, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 18);
+            this.label5.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1239, 680);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "N-Grams";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            ExeNgrams();
+            stopwatch.Stop();
+
+            string executionTime = $"Execution Time: {stopwatch.ElapsedMilliseconds} ms";
+            label5.Text = executionTime;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void ExeNgrams()
+        {
+
+            string[] dictionary1 = System.IO.File.ReadAllLines(@"BİLİM İŞ BAŞINDA.txt", System.Text.Encoding.GetEncoding("iso-8859-9"));
+            string[] dictionary2 = System.IO.File.ReadAllLines(@"BOZKIRDA.txt", System.Text.Encoding.GetEncoding("iso-8859-9"));
+            string[] dictionary3 = System.IO.File.ReadAllLines(@"DEĞİŞİM.txt", System.Text.Encoding.GetEncoding("iso-8859-9"));
+            string[] dictionary4 = System.IO.File.ReadAllLines(@"DENEMELER.txt", System.Text.Encoding.GetEncoding("iso-8859-9"));
+            string[] dictionary5 = System.IO.File.ReadAllLines(@"UNUTULMUŞ DİYARLAR.txt", System.Text.Encoding.GetEncoding("iso-8859-9"));
+
+            string diction1 = string.Join(" ", dictionary1);
+            string diction2 = string.Join(" ", dictionary2);
+            string diction3 = string.Join(" ", dictionary3);
+            string diction4 = string.Join(" ", dictionary4);
+            string diction5 = string.Join(" ", dictionary5);
+
+            List<string> dict1 = ParseDictionary(diction1);
+            List<string> dict2 = ParseDictionary(diction2);
+            List<string> dict3 = ParseDictionary(diction3);
+            List<string> dict4 = ParseDictionary(diction4);
+            List<string> dict5 = ParseDictionary(diction5);
+
+            List<string> unigram1 = GenerateNgrams(1, dict1);
+            List<string> bigram1 = GenerateNgrams(2, dict1);
+            List<string> trigram1 = GenerateNgrams(3, dict1);
+
+            List<string> unigram2 = GenerateNgrams(1, dict2);
+            List<string> bigram2 = GenerateNgrams(2, dict2);
+            List<string> trigram2 = GenerateNgrams(3, dict2);
+
+
+            List<string> unigram3 = GenerateNgrams(1, dict3);
+            List<string> bigram3 = GenerateNgrams(2, dict3);
+            List<string> trigram3 = GenerateNgrams(3, dict3);
+
+
+            List<string> unigram4 = GenerateNgrams(1, dict4);
+            List<string> bigram4 = GenerateNgrams(2, dict4);
+            List<string> trigram4 = GenerateNgrams(3, dict4);
+
+
+            List<string> unigram5 = GenerateNgrams(1, dict5);
+            List<string> bigram5 = GenerateNgrams(2, dict5);
+            List<string> trigram5 = GenerateNgrams(3, dict5);
+
+            label4.Text = "BİLİM İŞ BAŞINDA";
+            textboxFill(unigram1, bigram1, trigram1);
+
+        }
+        public void textboxFill(List<string> unigram, List<string> bigram, List<string> trigram)
+        {
+
+            foreach (var item in unigram)
+            {
+                textBox1.Text += item + Environment.NewLine;
+            }
+
+            foreach (var item in bigram)
+            {
+                textBox2.Text += item + Environment.NewLine;
+            }
+            foreach (var item in trigram)
+            {
+                textBox3.Text += item + Environment.NewLine;
+            }
+        }
+        public List<string> ParseDictionary(string dict)
+        {
+            List<string> dictionary = new List<string>();
+            string[] splitDictionary = dict.Split(' ');
+            for (int i = 0; i < splitDictionary.Length; i++)
+            {
+                string value = splitDictionary[i];
+                value = value.Trim();
+                value = value.Replace("\n", "");
+                if (!string.IsNullOrEmpty(value) && !value.Any(char.IsDigit))
+                {
+                    dictionary.Add(TrimPunctuation(value));
+                }
+            }
+            return dictionary;
+        }
+        static string TrimPunctuation(string value)
+        {
+            // Count start punctuation.
+            int removeFromStart = 0;
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (char.IsPunctuation(value[i]))
+                {
+                    removeFromStart++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // Count end punctuation.
+            int removeFromEnd = 0;
+            for (int i = value.Length - 1; i >= 0; i--)
+            {
+                if (char.IsPunctuation(value[i]))
+                {
+                    removeFromEnd++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            // No characters were punctuation.
+            if (removeFromStart == 0 &&
+                removeFromEnd == 0)
+            {
+                return value;
+            }
+            // All characters were punctuation.
+            if (removeFromStart == value.Length &&
+                removeFromEnd == value.Length)
+            {
+                return "";
+            }
+            // Substring.
+            return value.Substring(removeFromStart,
+                value.Length - removeFromEnd - removeFromStart);
+        }
+        private List<string> GenerateNgrams(int N, List<string> tokens)
+        {
+            List<string> ngramList = new List<string>();
+
+            //GENERATE THE N-GRAMS
+            for (int k = 0; k < (tokens.Count - N + 1); k++)
+            {
+                string s = "";
+                int start = k;
+                int end = k + N;
+                for (int j = start; j < end; j++)
+                {
+                    s = s + " " + tokens[j];
+                }
+                //Add n-gram to a list
+                ngramList.Add(s);
+            }
+      
+            return ngramList;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
+    }
+}
+
